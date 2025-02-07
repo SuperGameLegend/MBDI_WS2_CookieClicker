f2ff55fce3671499218e6e2321dea190c3976800
Emre <e.sagir@student.avans.nl>
2025-02-07 14:57:07 +0100

    Initial Commit

---
+++ MBDI_WS2_CookieClicker/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  MBDI_WS2_CookieClicker
+//
+//  Created by Emre Sağır on 07/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Cookie Clicker").font(.largeTitle)
+            Button( action: {
+                print("Clicked!")
+            },
+            label:{
+                Image("cookie").resizable(resizingMode: .stretch).aspectRatio(contentMode: .fit)
+            }
+            )
+            Text("Click on the cookie!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MBDI_WS2_CookieClickerTests/MBDI_WS2_CookieClickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MBDI_WS2_CookieClickerTests.swift
+//  MBDI_WS2_CookieClickerTests
+//
+//  Created by Emre Sağır on 07/02/2025.
+//
+
+import Testing
+@testable import MBDI_WS2_CookieClicker
+
+struct MBDI_WS2_CookieClickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
