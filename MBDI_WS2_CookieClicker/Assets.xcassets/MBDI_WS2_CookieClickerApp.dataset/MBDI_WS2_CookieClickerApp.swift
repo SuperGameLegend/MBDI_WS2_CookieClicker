624ef91313674841f375fe2cd3a9b49bde22688a
Emre <e.sagir@student.avans.nl>
2025-02-07 15:38:50 +0100

    Added views

---
+++ MBDI_WS2_CookieClicker/Assets.xcassets/MBDI_WS2_CookieClickerApp.dataset/MBDI_WS2_CookieClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBDI_WS2_CookieClickerApp.swift
+//  MBDI_WS2_CookieClicker
+//
+//  Created by Emre Sağır on 07/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct MBDI_WS2_CookieClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
