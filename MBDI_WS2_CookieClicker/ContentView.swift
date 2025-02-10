a196127354d128da780f6a736a94a0839e0f3fd7
Emre <e.sagir@student.avans.nl>
2025-02-10 14:57:42 +0100

    Store update

--- MBDI_WS2_CookieClicker/ContentView.swift
+++ MBDI_WS2_CookieClicker/ContentView.swift
@@ -8,7 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var upgrade1bought = false
+    @State var upgrade2bought = false
+    @State var upgrade3bought = false
+    @State var timerupgradebought =  false
     @State var timesClicked = 0
+    @State var isShowingSheet = false
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     var cookie =             Image("cookie").resizable(resizingMode: .stretch)
     var body: some View {
         VStack {
@@ -16,6 +22,15 @@ struct ContentView: View {
             Button( action: {
                 print("Clicked the cookie!")
                 timesClicked += 1
+                if(upgrade1bought){
+                    timesClicked += 1
+                }
+                if(upgrade2bought){
+                    timesClicked += 2
+                }
+                if(upgrade3bought){
+                    timesClicked += 5
+                }
                 
             },
             label:{
@@ -23,11 +38,38 @@ struct ContentView: View {
     
             }
             )
-            Text("You clicked the coockie " + String(timesClicked) + " times!")
+            
+            Text("You clicked the cookie " + String(timesClicked) + " times!")
+            
+                .sheet(isPresented:
+                        $isShowingSheet, content: {StoreView(timesClicked: $timesClicked, upgrade1bought : $upgrade1bought, upgrade2bought: $upgrade2bought, upgrade3bought: $upgrade3bought,
+                                                             timerupgradebought: $timerupgradebought)})
+            
+            Button( action: {
+                isShowingSheet = true
+            }
+            , label:{Text("Go to Store")})
+            
+            if(upgrade1bought){
+                Text("Upgrade 1 bought :)")
+            }
+            if(upgrade2bought){
+                Text("Upgrade 2 bought :)")
+            }
+            if(upgrade3bought){
+                Text("Upgrade 3 bought :)")
+            }
+            if(timerupgradebought){
+                Text("Timer upgrade active :)") .onReceive(timer) {_ in
+                    timesClicked += 10
+                }
+                
+            }
             
             NavigationView {
                 HStack{
-                    NavigationLink(destination: StoreView(timesClicked: .constant(10))){
+                    NavigationLink(destination: StoreView(timesClicked: $timesClicked, upgrade1bought : $upgrade1bought, upgrade2bought: $upgrade2bought, upgrade3bought: $upgrade3bought,
+                        timerupgradebought: $timerupgradebought)){
                         Text("Store")
                         
                     }
--- MBDI_WS2_CookieClicker/StoreView.swift
+++ MBDI_WS2_CookieClicker/StoreView.swift
@@ -9,12 +9,62 @@ import SwiftUI
 
 struct StoreView : View{
     @Binding var timesClicked: Int
+    @Binding var upgrade1bought : Bool
+    @Binding var upgrade2bought : Bool
+    @Binding var upgrade3bought : Bool
+    @Binding var timerupgradebought : Bool
+    
     var body: some View {
-
-        Text(String(timesClicked))
+        Text("You have " + String(timesClicked) + " cookies")
+        
+        if(!upgrade1bought){
+            Button( action: {
+                if(timesClicked >= 20 ){
+                    timesClicked -= 20
+                    upgrade1bought = true
+                }
+                
+            }, label:{Text("Upgrade 1: 20 cookies")})
+        }
+        
+        if(!upgrade2bought){
+            Button( action: {
+                if(timesClicked >= 100 ){
+                    timesClicked -= 100
+                    upgrade2bought = true
+                }
+                
+            }, label:{Text("Upgrade 2: 100 cookies")})
+        }
+        
+        if(!upgrade3bought){
+            Button( action: {
+                if(timesClicked >= 200 ){
+                    timesClicked -= 200
+                    upgrade3bought = true
+                }
+                
+            }, label:{Text("Upgrade 3: 200 cookies")})
+        }
+        
+        if(!timerupgradebought){
+            Button( action: {
+                if(timesClicked >= 500 ){
+                    timesClicked -= 500
+                    timerupgradebought = true
+                }
+                
+            }, label:{Text("Timer upgrade: 500 cookies")})
+        }
+        
+        if(upgrade1bought && upgrade2bought && upgrade3bought && timerupgradebought){
+            Text("All upgrades bought :D!")
+        }
+        
     }
 }
 
 #Preview {
-    StoreView(timesClicked: .constant(10))
+    StoreView(timesClicked: .constant(0), upgrade1bought: .constant(false), upgrade2bought: .constant(false), upgrade3bought: .constant(false),
+              timerupgradebought: .constant(false))
 }
